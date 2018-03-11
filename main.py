be34c5406cb4680cbd5e22706dff23beb905c910
agustinso <agustinmoralesdaniel@gmail.com>
2018-03-11 12:02:38 -0300

    changes to button logic

--- main.py
+++ main.py
@@ -41,10 +41,10 @@ class DataReader(threading.Thread):
         while not self.stopthread.isSet():
             try:
                 rslt = self.ser.read(num_bytes)  # Read serial data
-            except SerialException as _:
+            except SerialException:
                 self.error = True
                 self.stop()
-            
+
             # Convert serial data to array of numbers
             byte_array = unpack('%dB' % num_bytes, rslt)
 
@@ -67,17 +67,30 @@ class DataReader(threading.Thread):
                             self.data = roll(self.data, -1)
                             self.data[-1] = val
                             lock.release()
-        
+
         self.ser.close()
 
     def stop(self):
         self.stopthread.set()
 
+
 class Oscilloscope():
     def __init__(self):
         self.screen_widght = 773
         self.screen_height = 580
+
+        self.colors = {'background': (255, 255, 255),
+                       'foreground': (0, 0, 0),
+                       'panel_background': (230, 230, 230),
+                       'button_normal': (50, 50, 50),
+                       'button_pressed': (150, 150, 150),
+                       'text_especial': (100, 100, 100),
+                       'text_normal': (230, 230, 230)
+                      }
+
         self.hold = False
+        self.rect_filter = pygame.Rect(5, self.screen_height-95, 95, 30)
+        self.rect_hold = pygame.Rect(5, self.screen_height-55, 95, 30)
         self.screen = pygame.display.set_mode(
             (self.screen_widght, self.screen_height))
         self.icon = pygame.image.load('icon.png')
@@ -85,17 +98,23 @@ class Oscilloscope():
         pygame.display.set_caption("Osciloscopio")
         self.font = pygame.font.Font("font.ttf", 20)
         self.clock = pygame.time.Clock()
-        
+
         self.reader = DataReader()
         self.run()
-    
+
+    def button_pause(self):
+        self.hold = not self.hold
+
+    def button_filter(self):
+        self.reader.filter = not self.reader.filter
+
     def plot(self, x, y, xmin, xmax, ymin, ymax):
         h = self.screen_height - 100
         w = self.screen_widght
         x = array(x)
         y = array(y)
 
-        #Scale data
+        # Scale data
         xspan = abs(xmax-xmin)
         yspan = abs(ymax-ymin)
         xsc = 1.0*(w+1)/xspan
@@ -103,7 +122,7 @@ class Oscilloscope():
         xp = (x-xmin)*xsc
         yp = h-(y-ymin)*ysc
 
-        #Draw grid
+        # Draw grid
         for i in range(10):
             pygame.draw.line(self.screen, (220, 220, 220),
                              (0, int(h*0.1*i)), (w-1, int(h*0.1*i)), 1)
@@ -112,7 +131,7 @@ class Oscilloscope():
 
         pygame.draw.line(self.screen, (0, 0, 0), (0, h), (w, h), 1)
 
-        #Plot data
+        # Plot data
         for i in range(len(xp)-1):
             pygame.draw.line(self.screen, (0, 0, 0), (int(xp[i]), int(yp[i])),
                              (int(xp[i+1]), int(yp[i+1])), 1)
@@ -128,16 +147,23 @@ class Oscilloscope():
                 sys.exit(0)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
-                    self.hold = not self.hold
+                    self.button_pause()
                 if event.key == pygame.K_o:
-                    self.reader.filter = not self.reader.filter
+                    self.button_filter()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                if self.rect_filter.collidepoint(mouse_pos):
+                    self.button_filter()
+                elif self.rect_hold.collidepoint(mouse_pos):
+                    self.button_pause()
 
             self.screen.fill((255, 255, 255))
 
             if self.reader.error:
                 error_text = self.font.render(
                     "Dispositivo no encontrado, reintentando...", 1, (0, 0, 0))
-                self.screen.blit(error_text, (self.screen_widght/2-300, self.screen_height/2))
+                self.screen.blit(
+                    error_text, (self.screen_widght/2-300, self.screen_height/2))
 
                 self.reader = DataReader()
             else:
@@ -149,26 +175,49 @@ class Oscilloscope():
                     lock.release()
                 self.plot(x, y, 0, self.reader.data_buff_size, 0, 1024)
 
+                pygame.draw.rect(self.screen, self.colors['panel_background'], 
+                                 (0, self.screen_height-99, self.screen_widght, self.screen_height))
+
                 # Display voltage
                 remaped = remap_value(self.reader.data[-1], 0, 1023, 0, 5)
 
-                voltage_text = self.font.render("%.3f V" % remaped, 1, (0, 10, 10))
+                voltage_text = self.font.render(
+                    "%.3f V" % remaped, 1, (0, 10, 10))
 
                 if self.hold:
-                    hold_text = self.font.render("PAUSADO", 1, (0, 10, 10))
+                    hold_text = self.font.render("Pausado", 1, self.colors['text_especial'])
+                    hold_color = self.colors['button_pressed']
                 else:
-                    hold_text = hold_text = self.font.render("", 1, (0, 10, 10))
+                    hold_text = hold_text = self.font.render(
+                        "Pausar", 1, self.colors['text_normal'])
+                    hold_color = self.colors['button_normal']
                 if self.reader.filter:
-                    filter_text = self.font.render("FILTRANDO", 1, (0, 10, 10))
+                    filter_text = self.font.render(
+                        "Filtrando", 1, self.colors['text_especial'])
+                    filter_color = self.colors['button_pressed']
                 else:
-                    filter_text = self.font.render("", 1, (0, 10, 10))
+                    filter_text = self.font.render("Filtrar", 1, self.colors['text_normal'])
+                    filter_color = self.colors['button_normal']
 
-                self.screen.blit(voltage_text, (self.screen_widght-60, self.screen_height-95))
-                self.screen.blit(filter_text, (5, self.screen_height-95))
-                self.screen.blit(hold_text, (5, self.screen_height-75))
+                hold_text_rect = hold_text.get_rect()
+                hold_text_rect.center = self.rect_hold.center
+
+                filter_text_rect = filter_text.get_rect()
+                filter_text_rect.center = self.rect_filter.center
+
+                self.screen.blit(
+                    voltage_text, (self.screen_widght-60, self.screen_height-95))
+
+                pygame.draw.rect(self.screen, filter_color,
+                                 self.rect_filter)
+                pygame.draw.rect(self.screen, hold_color, self.rect_hold)
+
+                self.screen.blit(filter_text, filter_text_rect)
+                self.screen.blit(hold_text, hold_text_rect)
 
             pygame.display.flip()
             self.clock.tick(0)
+
 
 if __name__ == "__main__":
     global lock
